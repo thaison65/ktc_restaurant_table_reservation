b05634c413130d8fdef08b66f0d5bf2640eddfc4
nghiemv2001 <nghiemV2001@gmail.com>
2024-08-19 16:53:33 +0700

    dialog fill infomation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './App.css';
+import HomePage from './pages/HomePage/HomePage';
 
 function App() {
   return (
-    <>
-      <h1>Hello</h1>
-    </>
+  <HomePage>
+  </HomePage>
+  
   );
 }
 
---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,19 @@
+// Move me to Profile.js!
+export function Profile() {
+    return (
+      <img
+        src="https://i.imgur.com/QIrZWGIs.jpg"
+        alt="Alan L. Hart"
+      />
+    );
+  }
+  
+  export default function Gallery() {
+    return (
+      <section>
+        <h1>Amazing scientists</h1>
+        <Profile />
+      </section>
+    );
+  }
+  
